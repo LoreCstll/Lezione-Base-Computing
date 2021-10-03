3cbf468ce09e96a49f6f9fedebefa4a7b8d43d1f
Lorenzo <lorenz_gauge@pop-os.localdomain>
2021-10-03 11:48:32 +0200

    Initial Import

--- Primo_assegnamento.py
+++ Primo_assegnamento.py
@@ -1,2 +1,15 @@
-#ancora non so cosa farci
+import argparse
+parser = argparse.ArgumentParser(description='Book structure.')
+parser.add_argument('path',help='path to the input file')
+#parser.add_argument("-v", "--verbosity", action="count", default=0, help = 'add comments to the results')
+args = parser.parse_args()
+var_lettura = open(args.path, "r").read()
+lower_var = var_lettura.lower()
+#count = lower_var.count("a")
+#print(count)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',  'z']
 
+
+for i in alphabet:
+   print('La lettera %s è ripetuta %i nel libro' %(i,lower_var.count(i)))
+   
