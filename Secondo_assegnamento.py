54e1caee23440bfee713b53b1d2b6f194d35e370
Matteoparri <79097501+Matteoparri@users.noreply.github.com>
2021-10-13 22:36:31 +0200

    changed some shit on second_assignment
    c'erano errori

--- Secondo_assegnamento.py
+++ Secondo_assegnamento.py
@@ -2,7 +2,7 @@ import math
 
 class Particle:
 	""" Class representing a generic particle """
-	
+
 	def __init__(self, mass, charge, name, momentum =0.):
 		""" Arguments:
 		mass : mass particle in MeV/c^2
@@ -10,17 +10,17 @@ class Particle:
 		name: particle name
 		momentum: module of the particle initial momentum
 		"""
-	
+
 		self.mass = mass
 		self.charge = charge
-		self.name = name 
-		self.momentum = momentum  
-		
+		self.name = name
+		self.momentum = momentum
+
 	def info(self):
 		""" Print some info about the particle
 		"""
 		msg = 'Particle "{}" of mass {:.3f} MeV/c^2, charge :{}'
-		return msg.format(name = self.name, mass= self.mass, charge = self.charge)
+		return msg.format(self.name, self.mass, self.charge)
 
 	@property
 	def energy(self):
@@ -36,27 +36,32 @@ class Particle:
 			print('Cannot set energy to a value lower than mass')
 		else:
 			self.momentum = math.sqrt(energy**2 - self.mass**2)
-	
+
 	@property
 	def momentum(self):
-		return self.momentum
-	
+		return self._momentum
+
 	@momentum.setter
 	def momentum(self, momentum):
 		if momentum<0:
 			print('momoentum non acceptable, cant set at a negative value')
+			print('momentum will be set to 0')
+			self._momentum = 0.
 		else:
-			self.momentum = momentum
-	
+			self._momentum = momentum
+
 
 particle = Particle(105.6, charge = -1, name = 'Muon')
 print(particle.info())
-print("Particle energy: {:.2f} MeV", particle.energy)
+print(f'Particle energy: {particle.energy:.2f} MeV')
+
+
+
 """
 partucle.energy = 200
 print("Particle energy: {:.2f} MeV", particle.energy)
 print(particle.momentum)
-particle.momentum = 20 
+particle.momentum = 20
 print(particle.momentum)
 
 proton = particle(939, charge = +1, name = 'Proton', momentum = 10)
